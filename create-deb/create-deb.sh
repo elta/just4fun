5c3255016a99a4ca8ec829e2c8cf708a0a43142d
Dongxue Zhang <elta.era@gmail.com>
2013-06-27 16:23:35 +0800

    [create-deb] Simple example of how to create deb package
      We can create a simple deb package in create-deb dir.
    By run create-deb.sh, it will create atest_0.1-1_amd64.deb. The deb
    will install a.sh to /usr/bin, then tape a.sh, will get message: "Hello deb".
    
      Install deb: sudo dpkg -i atest_0.1-1_amd64.deb
      Remove deb: sudo dpkg -r atest

---
+++ create-deb/create-deb.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# create a tmp dir, do all thins in it.
+mkdir -pv tmp
+cd tmp
+
+# create atest-0.1 dir, and touch files in it.
+mkdir -pv atest-0.1
+cd atest-0.1
+cat > Makefile << EOF
+# Makefile
+
+all:
+
+install:
+	mkdir -pv \$(DESTDIR)/usr/bin
+	cp a.sh \$(DESTDIR)/usr/bin/a.sh
+
+clean:
+destclean:
+	rm \$(DESTDIR)/usr/bin/a.sh
+
+EOF
+
+cat > a.sh << EOF
+#! /bin/bash
+
+echo "Hello deb"
+
+EOF
+
+chmod +x a.sh
+
+cd ..
+
+# tar
+tar cvf atest-0.1.tar.gz atest-0.1
+
+# Begin build deb
+cd atest-0.1
+echo "" | dh_make -s -e "Name Family facker@fake.com" -f ../atest-0.1.tar.gz
+
+dpkg-buildpackage -rfakeroot -b -us -uc
+
+cd ..
+
+cp *.deb ../
